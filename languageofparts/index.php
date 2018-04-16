8944aa154a526551a97ba2759e09b2348f5416b8
gareth85 <38084626+gareth85@users.noreply.github.com>
2018-04-16 16:25:53 +0100

    Update index.php

--- languageofparts/index.php
+++ languageofparts/index.php
@@ -8,12 +8,37 @@
 
 <h1>Language of Parts Demo.</h1>
 
-<h2>Languages NOT DEFINED.<h2>
+<h2>Languages NOT DEFINED.</h2>
 
 <h3>English</h3>
 
 <p>“Atticus said to Jem one day, “I’d rather you shot at tin cans in the backyard, but I know you’ll go after birds. Shoot all the blue jays you want, if you can hit ‘em, but remember it’s a sin to kill a mockingbird.” That was the only time I ever heard Atticus say it was a sin to do something, and I asked Miss Maudie about it. “Your father’s right,” she said. “Mockingbirds don’t do one thing except make music for us to enjoy. They don’t eat up people’s gardens, don’t nest in corn cribs, they don’t do one thing but sing their hearts out for us. That’s why it’s a sin to kill a mockingbird.” – Harper Lee, To Kill a Mockingbird</p>
 
+<h3>Français</h3>
+
+<p>“Atticus dit à Jem un jour,” Je préférerais que vous tiriez sur des boîtes de conserve dans l’arrière-cour, mais je sais que vous poursuivrez les oiseaux. Tue tous les geais bleus que tu veux, si tu peux les frapper, mais souviens-toi que c’est un péché de tuer un oiseau moqueur. C’était la seule fois que j’entendais Atticus dire que c’était un péché de faire quelque chose. il. “Ton père a raison,” dit-elle. “Les oiseaux moqueurs ne font qu’une chose, sauf faire de la musique pour que nous puissions en profiter. Ils ne mangent pas les jardins des gens, ne nichent pas dans des crèches de maïs, ils ne font pas une chose mais chanter leurs coeurs pour nous. C’est pourquoi c’est un péché de tuer un oiseau moqueur. “- Harper Lee, To Kill a Mockingbird</p>
+
+<h3>Espanol</h3>
+
+<p>“Atticus le dijo a Jem un día,” Preferiría que le dispararas a las latas en el patio trasero, pero sé que irás tras las aves. Dispara a todos los arrendajos azules que quieras, si puedes golpearlos, pero recuerda que es un pecado matar a un ruiseñor. “Esa fue la única vez que escuché a Atticus decir que era un pecado hacer algo, y le pregunté a la señorita Maudie sobre eso. “Tu padre tiene razón”, dijo. “Los sinsontes no hacen otra cosa que hacer música para que la disfrutemos. No se comen los jardines de las personas, no anidan en cunas de maíz, no hacen otra cosa que cantar sus corazones por nosotros. Es por eso que es un pecado matar a un ruiseñor. “- Harper Lee, To Kill a Mockingbird
+Languages DEFINED.</p>
+
+<hr>
+
+<h2>Languages DEFINED.</h2>
+
+<h3>English</h3>
+
+<p>“Atticus said to Jem one day, “I’d rather you shot at tin cans in the backyard, but I know you’ll go after birds. Shoot all the blue jays you want, if you can hit ‘em, but remember it’s a sin to kill a mockingbird.” That was the only time I ever heard Atticus say it was a sin to do something, and I asked Miss Maudie about it. “Your father’s right,” she said. “Mockingbirds don’t do one thing except make music for us to enjoy. They don’t eat up people’s gardens, don’t nest in corn cribs, they don’t do one thing but sing their hearts out for us. That’s why it’s a sin to kill a mockingbird.” – Harper Lee, To Kill a Mockingbird</p>
+
+<h3>Français</h3>
+
+<p>“Atticus dit à Jem un jour,” Je préférerais que vous tiriez sur des boîtes de conserve dans l’arrière-cour, mais je sais que vous poursuivrez les oiseaux. Tue tous les geais bleus que tu veux, si tu peux les frapper, mais souviens-toi que c’est un péché de tuer un oiseau moqueur. C’était la seule fois que j’entendais Atticus dire que c’était un péché de faire quelque chose. il. “Ton père a raison,” dit-elle. “Les oiseaux moqueurs ne font qu’une chose, sauf faire de la musique pour que nous puissions en profiter. Ils ne mangent pas les jardins des gens, ne nichent pas dans des crèches de maïs, ils ne font pas une chose mais chanter leurs coeurs pour nous. C’est pourquoi c’est un péché de tuer un oiseau moqueur. “- Harper Lee, To Kill a Mockingbird</p>
+
+<h3>Espanol</h3>
+
+<p>“Atticus le dijo a Jem un día,” Preferiría que le dispararas a las latas en el patio trasero, pero sé que irás tras las aves. Dispara a todos los arrendajos azules que quieras, si puedes golpearlos, pero recuerda que es un pecado matar a un ruiseñor. “Esa fue la única vez que escuché a Atticus decir que era un pecado hacer algo, y le pregunté a la señorita Maudie sobre eso. “Tu padre tiene razón”, dijo. “Los sinsontes no hacen otra cosa que hacer música para que la disfrutemos. No se comen los jardines de las personas, no anidan en cunas de maíz, no hacen otra cosa que cantar sus corazones por nosotros. Es por eso que es un pecado matar a un ruiseñor. “- Harper Lee, To Kill a Mockingbird
+Languages DEFINED.</p>
 
 </body>
 
